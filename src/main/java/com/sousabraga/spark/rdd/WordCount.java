8ec14f631119ea03c612d108280955a2ae75a471
Matheus Braga <matheusssbraga@gmail.com>
2020-01-02 21:43:26 +0000

    First example with RDDs to count words from a file

---
+++ src/main/java/com/sousabraga/spark/rdd/WordCount.java
@@ -0,0 +1,25 @@
+package com.sousabraga.spark.rdd;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class WordCount {
+
+	public static void main(String[] args) throws Exception {
+		SparkConf conf = new SparkConf().setAppName("wordCounts").setMaster("local[3]");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD<String> lines = sc.textFile("in/word_count.text");
+        JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
+
+        Map<String, Long> wordCounts = words.countByValue();
+
+        for (Map.Entry<String, Long> entry : wordCounts.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+	}
+	
+}
